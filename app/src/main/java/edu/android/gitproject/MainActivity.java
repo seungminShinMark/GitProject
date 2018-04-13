fd07a36ad692d3647babd220f5ce6f31e6dfee90
seungminShinMark <38124193+seungminShinMark@users.noreply.github.com>
2018-04-13 13:41:32 +0900

    Update MainActivity.java
    find button / Simulation

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        
         textView = findViewById(R.id.textView);
     }
 }
